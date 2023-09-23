5e41c7409d577be0f24b62f84fc6e283a3eae271
Dheeraj Tanwar <dheerajtanwar640@gmail.com>
2023-09-23 05:56:06 +0000

    Datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,20 @@
+/*
+Javascript has mainly 8 datatypes which are categorise into two category.
+Primitive Datatypes: Number, String, Boolean, Bigint, Undefined, Null Symbol
+Non-Primitive Datatype: Object
+*/
+
+let name = 'Dheeraj'//String
+let age = 18 // Number
+let income = 999999999999999n //Bigint
+let isLoggedIn = true // Boolean
+let accountStatus // Undefined
+let email = null // Null
+
+let information = {
+    name : 'Dheeraj',
+    age : '18',
+    occupation : 'Business'
+} //Object
+
+console.log(typeof(income));

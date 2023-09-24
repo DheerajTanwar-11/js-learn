049dd0499bd07281967ec2792f04a376271cf323
Dheeraj Tanwar <dheerajtanwar640@gmail.com>
2023-09-24 11:19:27 +0000

    Math and Number

---
+++ 01_Basics/05_Math_and_num.js
@@ -0,0 +1,18 @@
+// Numbers in Javascript
+
+const score = 400
+let balance = new Number(100)
+console.log(balance)
+
+const otherNumber = 123.888
+
+console.log(otherNumber.toFixed())
+console.log(otherNumber.toPrecision(1))
+console.log(otherNumber.toString())
+
+// Math in javascript. Math is an object in javascript containing many functions.
+
+let number = Math.random()
+console.log(number)
+
+console.log(Math.floor((Math.random()* 10) + 1))
